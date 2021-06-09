749c95efc1a1cca4e1f2c0959691a1518ee27791
cody-mckeon <codymckeon@gmail.com>
2021-06-09 12:46:01 -0700

    Spread Operator to reduce code

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["lime", "Lemon", "Orange"];
+const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+
+const fullName = {
+  fName: "James",
+  lName: "Bond"
+};
+
+const user = {
+  ...fullName,
+  id: 1,
+  username: "Jamesbond007"
+};
